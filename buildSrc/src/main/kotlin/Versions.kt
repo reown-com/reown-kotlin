974a28bc2123e613a44117d75544dee655827a7c
kubel <jakubwitczak6@gmail.com>
2024-10-30 12:07:34 +0100

    Bump Yttrium

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -13,7 +13,7 @@ const val NOTIFY_VERSION = "1.0.1"
 const val WALLETKIT_VERSION = "1.0.1"
 const val APPKIT_VERSION = "1.0.1"
 const val MODAL_CORE_VERSION = "1.0.1"
-const val YTTRIUM_VERSION = "0.2.3"
+const val YTTRIUM_VERSION = "0.2.4"
 
 //Artifact ids
 const val ANDROID_BOM = "android-bom"
