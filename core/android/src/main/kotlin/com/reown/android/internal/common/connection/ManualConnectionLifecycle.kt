180215c63c2d65af27f87745cfbfd87bcfd0b198
panekzogen <ekzogen7@gmail.com>
2025-03-21 10:21:48 +0100

    Make manual connection accessible and manageable

--- core/android/src/main/kotlin/com/reown/android/internal/common/connection/ManualConnectionLifecycle.kt
+++ core/android/src/main/kotlin/com/reown/android/internal/common/connection/ManualConnectionLifecycle.kt
@@ -7,23 +7,26 @@ import com.tinder.scarlet.lifecycle.LifecycleRegistry
 import com.reown.foundation.network.ConnectionLifecycle
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-internal class ManualConnectionLifecycle(
+class ManualConnectionLifecycle(
     private val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(),
 ) : Lifecycle by lifecycleRegistry, ConnectionLifecycle {
+
+    private val _onResume = MutableStateFlow<Boolean?>(null)
+    override val onResume: StateFlow<Boolean?> = _onResume.asStateFlow()
+
     fun connect() {
         lifecycleRegistry.onNext(Lifecycle.State.Started)
+        _onResume.value = true
     }
 
     fun disconnect() {
         lifecycleRegistry.onNext(Lifecycle.State.Stopped.WithReason())
+        _onResume.value = false
     }
-
-    override val onResume: StateFlow<Boolean?>
-        get() = MutableStateFlow(null)
-
     override fun reconnect() {
-        lifecycleRegistry.onNext(Lifecycle.State.Stopped.WithReason())
-        lifecycleRegistry.onNext(Lifecycle.State.Started)
+        disconnect()
+        connect()
     }
 }
