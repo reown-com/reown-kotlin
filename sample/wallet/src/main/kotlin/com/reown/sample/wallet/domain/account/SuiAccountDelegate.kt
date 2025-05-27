288e8688b212215f1052e43e1973edfea620e801
jakubuid <jakubwitczak6@gmail.com>
2025-05-27 10:56:48 +0200

    add sui keys

--- sample/wallet/src/main/kotlin/com/reown/sample/wallet/domain/account/SuiAccountDelegate.kt
+++ sample/wallet/src/main/kotlin/com/reown/sample/wallet/domain/account/SuiAccountDelegate.kt
@@ -1,7 +1,53 @@
 package com.reown.sample.wallet.domain.account
 
-object SuiAccountDelegate {
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import com.reown.sample.wallet.ui.routes.dialog_routes.transaction.Chain
+import com.reown.walletkit.utils.sui.SuiUtils
 
-    fun storeAccount() {
+object SuiAccountDelegate {
+    lateinit var application: Application
+    private val sharedPreferences: SharedPreferences by lazy { application.getSharedPreferences("Wallet_Sample_Shared_Prefs", Context.MODE_PRIVATE) }
+    private const val KEY_PAIR_TAG = "self_sui_key_pair"
+
+    private val isInitialized: Boolean get() = sharedPreferences.getString(KEY_PAIR_TAG, null) != null
+
+    private fun storeAccount(keyPair: String? = null): String =
+        if (keyPair == null) {
+            SuiUtils.generateKeyPair()
+                .also { generatedKeyPair ->
+                    sharedPreferences.edit { putString(KEY_PAIR_TAG, generatedKeyPair) }
+                }
+        } else {
+            sharedPreferences.edit { putString(KEY_PAIR_TAG, keyPair) }
+            keyPair
+        }
+
+    var keypair: String
+        get() = if (isInitialized) sharedPreferences.getString(KEY_PAIR_TAG, null)!! else storeAccount()
+        set(value) {
+            storeAccount(value)
+        }
+
+    val address: String
+        get() = getSuiAddressForKeyPair(keypair)
+
+    val publicKey: String
+        get() = getSuiPublicKeyForKeyPair(keypair)
+
+    val caip10Address: String
+        get() = "${Chain.SUI.id}:$address"
+
+
+    private fun getSuiAddressForKeyPair(keyPair: String? = null): String {
+        val currentKeyPair = keyPair ?: sharedPreferences.getString(KEY_PAIR_TAG, null)!!
+        return SuiUtils.getAddressFromKeyPair(currentKeyPair)
+    }
+
+    private fun getSuiPublicKeyForKeyPair(keyPair: String? = null): String {
+        val currentKeyPair = keyPair ?: sharedPreferences.getString(KEY_PAIR_TAG, null)!!
+        return SuiUtils.getPublicKeyFromKeyPair(currentKeyPair)
     }
 }
