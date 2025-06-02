19b23ca43f10130a2b85818304fe0eba709634f6
jakubuid <jakubwitczak6@gmail.com>
2025-06-02 17:06:46 +0200

    fix android tls issue

--- product/walletkit/src/main/kotlin/com/reown/walletkit/utils/sui/Sui.kt
+++ product/walletkit/src/main/kotlin/com/reown/walletkit/utils/sui/Sui.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import com.yttrium.YttriumKt
 import uniffi.yttrium.PulseMetadata
 import uniffi.yttrium.SuiClient
-import uniffi.yttrium.fundingMetadataToAmount
 import uniffi.yttrium.initializeAndroidTls
 import uniffi.yttrium.suiGenerateKeypair
 import uniffi.yttrium.suiGetAddress
@@ -25,9 +24,6 @@ object SuiUtils {
                 url = null
             )
         )
-
-//        Java_com_yttrium_YttriumKt_initializeTls()
-//        initializeAndroidTls()
     }
 
     suspend fun signAndExecuteTransaction(chaiId: String, keyPair: String, txData: ByteArray): String {
