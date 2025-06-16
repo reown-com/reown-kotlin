2c37bd4ebf4e962774ca12dcf595efafa43a6367
jakubuid <jakubwitczak6@gmail.com>
2025-06-16 10:07:51 +0200

    clean up

--- protocol/sign/src/main/kotlin/com/reown/sign/engine/model/tvf/TVF.kt
+++ protocol/sign/src/main/kotlin/com/reown/sign/engine/model/tvf/TVF.kt
@@ -152,10 +152,6 @@ internal class TVF(private val moshi: Moshi) {
                         }
                 }
 
-                POLKADOT_SIGN_TRANSACTION -> {
-                    listOf("")
-                }
-
                 else -> null
             }
         } catch (e: Exception) {
