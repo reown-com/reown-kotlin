9a65f463251cdca42b342e9740a1fbcb6a718372
jakubuid <jakubwitczak6@gmail.com>
2025-08-20 09:10:42 +0200

    add pos module

---
+++ product/pos/build.gradle.kts
@@ -0,0 +1,71 @@
+plugins {
+    id("com.android.library")
+    id(libs.plugins.kotlin.android.get().pluginId)
+    alias(libs.plugins.google.ksp)
+    id("publish-module-android")
+    id("jacoco-report")
+}
+
+project.apply {
+    extra[KEY_PUBLISH_ARTIFACT_ID] = POS
+    extra[KEY_PUBLISH_VERSION] = POS_VERSION
+    extra[KEY_SDK_NAME] = "pos"
+}
+
+android {
+    namespace = "com.reown.pos"
+    compileSdk = COMPILE_SDK
+
+    defaultConfig {
+        minSdk = MIN_SDK
+
+        aarMetadata {
+            minCompileSdk = MIN_SDK
+        }
+
+        buildConfigField(type = "String", name = "SDK_VERSION", value = "\"${requireNotNull(extra.get(KEY_PUBLISH_VERSION))}\"")
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    lint {
+        abortOnError = true
+        ignoreWarnings = true
+        warningsAsErrors = false
+    }
+
+    compileOptions {
+        sourceCompatibility = jvmVersion
+        targetCompatibility = jvmVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.appCompat)
+    implementation(libs.androidx.material)
+
+    testImplementation(libs.jUnit)
+
+    androidTestImplementation(libs.mockk.android)
+    androidTestImplementation(libs.coroutines.test)
+    androidTestImplementation(libs.core)
+
+    androidTestUtil(libs.androidx.testOrchestrator)
+    androidTestImplementation(libs.bundles.androidxAndroidTest)
+}
