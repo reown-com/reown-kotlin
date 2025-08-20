a172a865c88918f0e8d4cdb9dcca8cf04473af70
jakubuid <jakubwitczak6@gmail.com>
2025-08-20 15:15:19 +0200

    core POS flow logic

--- product/pos/src/main/kotlin/com/reown/pos/client/POS.kt
+++ product/pos/src/main/kotlin/com/reown/pos/client/POS.kt
@@ -1,6 +1,5 @@
 package com.reown.pos.client
 
-import com.reown.android.CoreInterface
 import java.net.URI
 
 /**
@@ -27,23 +26,33 @@ object POS {
             val events: List<String>
         ) : Model()
 
-        data class PaymentEvent(val event: String) : Model()
-
-        sealed interface PaymentResult {
-            data class Success(val txHash: String, val receipt: String) : PaymentResult
-            data class Failure(val error: PosError) : PaymentResult
+        sealed interface PaymentEvent {
+            data class QrReady(val uri: URI) : PaymentEvent
+            data object Connected : PaymentEvent
+            data object ConnectedRejected : PaymentEvent
+            data class ConnectionFailed(val error: Throwable) : PaymentEvent
+            data object PaymentRequested : PaymentEvent
+            data object PaymentBroadcasted : PaymentEvent
+            data class PaymentRejected(val error: PosError.RejectedByUser) : PaymentEvent
+            data class PaymentSuccessful(val txHash: String, val receipt: String) : PaymentEvent
+            data class Error(val error: PosError.General) : PaymentEvent
         }
 
         sealed interface PosError {
             data object Timeout : PosError
-            data object RejectedByUser : PosError
+            data class RejectedByUser(val message: String) : PosError
             data class Backend(val code: Int, val message: String) : PosError
             data object Network : PosError
             data object WalletUnsupported : PosError
-            data class Unknown(val cause: Throwable?) : PosError
+            data class General(val cause: Throwable?) : PosError
         }
 
-        data class PaymentIntent(val token: String, val amount: String, val chainId: String, val recipient: String) : Model()
+        data class PaymentIntent(
+            val token: String,
+            val amount: String,
+            val chainId: String,
+            val recipient: String
+        ) : Model()
     }
 
     /**
--- product/pos/src/main/kotlin/com/reown/pos/client/POSClient.kt
+++ product/pos/src/main/kotlin/com/reown/pos/client/POSClient.kt
@@ -4,14 +4,20 @@ import android.app.Application
 import com.reown.android.Core
 import com.reown.android.CoreInterface
 import com.reown.android.CoreProtocol
+import com.reown.android.internal.common.scope
 import com.reown.android.relay.ConnectionType
 import com.reown.sign.client.Sign
 import com.reown.sign.client.SignClient
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
+import java.net.URI
 
 object POSClient {
     private lateinit var coreClient: CoreInterface
+    private lateinit var posDelegate: POSDelegate
     private val sessionNamespaces = mutableMapOf<String, POS.Model.Namespace>()
-    lateinit private var posDelegate: POSDelegate
+    private var paymentIntents: List<POS.Model.PaymentIntent> = emptyList()
 
     /**
      * POS delegate interface for handling events
@@ -59,24 +65,61 @@ object POSClient {
     /**
      * Set the chain to use, EVM only
      */
-    fun setChain(chainIds: List<String>) {
-        sessionNamespaces["eip155"] = POS.Model.Namespace(
-            chains = chainIds,
-            methods = listOf("eth_sendTransaction"),
-            events = listOf("chainChanged", "accountsChanged")
-        )
+    @Throws(IllegalStateException::class)
+    fun setChains(chainIds: List<String>) {
+        if (chainIds.any { chainId -> chainId.startsWith("eip155") }) {
+            sessionNamespaces["eip155"] = POS.Model.Namespace(
+                chains = chainIds,
+                methods = listOf("eth_sendTransaction"),
+                events = listOf("chainChanged", "accountsChanged")
+            )
+        } else {
+            throw IllegalStateException("EVM only")
+        }
     }
 
     /**
      * Create a payment intent
      */
-    fun createPaymentIntent(paymentIntents: List<POS.Model.PaymentIntent>) {
-//        - Generates the connection URL
-//        - Sends the connection proposal to the wallet
-//        - Awaits the connection result
-//        - Builds and sends the transaction to the wallet
-//        - Awaits the transaction result from the wallet
-//        - Checks the transaction status
+    @Throws(IllegalStateException::class)
+    fun createPaymentIntent(intents: List<POS.Model.PaymentIntent>) {
+        //TODO: add intent fields validation
+        checkPOSDelegateInitialization()
+        if (sessionNamespaces.isEmpty()) throw IllegalStateException("No chain set, call setChains method first")
+        if (intents.isEmpty()) throw IllegalStateException("No payment intents provided")
+        paymentIntents = intents
+
+
+        val pairing = coreClient.Pairing.create { error ->
+            posDelegate.onEvent(POS.Model.PaymentEvent.ConnectionFailed(error.throwable))
+        }
+
+        if (pairing != null) {
+            val signNamespaces = sessionNamespaces.mapValues { (_, namespace) ->
+                Sign.Model.Namespace.Proposal(
+                    chains = namespace.chains,
+                    methods = namespace.methods,
+                    events = namespace.events
+                )
+            }
+
+            val connectParams = Sign.Params.ConnectParams(
+                sessionNamespaces = signNamespaces,
+                pairing = pairing
+            )
+
+            SignClient.connect(
+                connectParams = connectParams,
+                onSuccess = { url ->
+                    posDelegate.onEvent(POS.Model.PaymentEvent.QrReady(URI(pairing.uri)))
+                },
+                onError = { error ->
+                    posDelegate.onEvent(POS.Model.PaymentEvent.ConnectionFailed(error.throwable))
+                }
+            )
+        } else {
+            posDelegate.onEvent(POS.Model.PaymentEvent.ConnectionFailed(Throwable("Pairing is null")))
+        }
     }
 
     /**
@@ -84,21 +127,107 @@ object POSClient {
      */
     fun setDelegate(delegate: POSDelegate) {
         posDelegate = delegate
+
         val dappDelegate = object : SignClient.DappDelegate {
+
             override fun onSessionApproved(approvedSession: Sign.Model.ApprovedSession) {
-                TODO("Not yet implemented")
+                scope.launch {
+                    supervisorScope {
+                        posDelegate.onEvent(POS.Model.PaymentEvent.Connected)
+                        val method = sessionNamespaces.values.first().methods.first()
+                        val chainId = paymentIntents.first().chainId
+                        val amount = paymentIntents.first().amount
+                        val token = paymentIntents.first().token
+                        val recipient = paymentIntents.first().recipient
+                        var senderAddress: String? = null
+
+                        //TODO: Build Request using server
+                        print("kobe: Building request: $chainId, $amount, $token, $recipient ")
+                        delay(2000)
+                        print("kobe: Request built success")
+
+
+                        approvedSession.namespaces.forEach { (namespace, session) ->
+                            // Check if the namespace key matches the chain ID from payment intent
+                            senderAddress = when {
+                                // If chains are not null and not empty, find the first account on the same chain as payment intent
+                                session.chains != null && session.chains!!.isNotEmpty() -> {
+                                    val chains = session.chains
+                                    if (chains != null) {
+                                        session.accounts.firstOrNull { account ->
+                                            chains.any { chain ->
+                                                chain == chainId || account.startsWith("$chain:")
+                                            }
+                                        }
+                                    } else {
+                                        null
+                                    }
+                                }
+
+                                namespace == chainId -> {
+                                    session.accounts.firstOrNull()
+                                }
+
+                                else -> null
+                            }
+                        }
+
+                        if (senderAddress != null) {
+                            val request = Sign.Params.Request(
+                                sessionTopic = approvedSession.topic,
+                                method = method,
+                                params = "[{\"from\":\"$senderAddress\",\"to\":\"$recipient\",\"data\":\"0x\",\"gasLimit\":\"0x5208\",\"gasPrice\":\"0x0649534e00\",\"value\":\"$amount\",\"nonce\":\"0x07\"}]",
+                                chainId = chainId
+                            )
+
+                            SignClient.request(
+                                request = request,
+                                onSuccess = { sentRequest -> posDelegate.onEvent(POS.Model.PaymentEvent.PaymentRequested) },
+                                onError = { error -> posDelegate.onEvent(POS.Model.PaymentEvent.ConnectionFailed(error.throwable)) }
+                            )
+
+                        } else {
+                            //TODO: disconnect?
+                            posDelegate.onEvent(POS.Model.PaymentEvent.Error(POS.Model.PosError.General(Throwable("No matching account found"))))
+                        }
+                    }
+                }
             }
 
             override fun onSessionRejected(rejectedSession: Sign.Model.RejectedSession) {
-                TODO("Not yet implemented")
+                posDelegate.onEvent(POS.Model.PaymentEvent.ConnectedRejected)
             }
 
             override fun onSessionRequestResponse(response: Sign.Model.SessionRequestResponse) {
-                TODO("Not yet implemented")
+                when (val result = response.result) {
+                    is Sign.Model.JsonRpcResponse.JsonRpcResult -> {
+                        scope.launch {
+                            supervisorScope {
+                                posDelegate.onEvent(POS.Model.PaymentEvent.PaymentBroadcasted)
+
+                                // TODO: Check transaction status on blockchain, handle server errors and timeout
+                                print("kobe: Checking payment status..")
+                                delay(2000)
+                                print("kobe: Payment successful")
+
+                                val txHash = result.toString()
+                                //TODO: get txHash and receipt from server
+                                posDelegate.onEvent(POS.Model.PaymentEvent.PaymentSuccessful(txHash = txHash, receipt = "test"))
+
+                                //TODO: disconnect session when payment is successful
+                            }
+                        }
+                    }
+
+                    is Sign.Model.JsonRpcResponse.JsonRpcError -> {
+                        val error = POS.Model.PosError.RejectedByUser(message = result.message)
+                        posDelegate.onEvent(POS.Model.PaymentEvent.PaymentRejected(error))
+                    }
+                }
             }
 
             override fun onError(error: Sign.Model.Error) {
-                TODO("Not yet implemented")
+                posDelegate.onEvent(POS.Model.PaymentEvent.Error(POS.Model.PosError.General(error.throwable)))
             }
 
             override fun onConnectionStateChange(state: Sign.Model.ConnectionState) {}
@@ -117,5 +246,11 @@ object POSClient {
         }
 
         SignClient.setDappDelegate(dappDelegate)
+    }
+
+    private fun checkPOSDelegateInitialization() {
+        check(::posDelegate.isInitialized) {
+            "POSDelegate needs to be initialized first"
+        }
     }
 }
