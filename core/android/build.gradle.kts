80d27f4d7aacbbb347865003cb274b545ffdcaae
jakubuid <jakubwitczak6@gmail.com>
2025-11-05 12:33:19 +0100

    add x86_64 support

--- core/android/build.gradle.kts
+++ core/android/build.gradle.kts
@@ -98,16 +98,6 @@ dependencies {
     debugApi(project(":foundation"))
     releaseApi("com.reown:foundation:$FOUNDATION_VERSION")
 
-    // Use specific yttrium version for CI builds, default version for local builds
-    // Release builds use stable version, regular CI builds use CI version for e2e tests
-    val yttriumVersion = if (System.getenv("IS_RELEASE_BUILD") == "true") {
-        "0.9.69" // Use stable version for release builds
-    } else if (System.getenv("CI") == "true") {
-        System.getenv("YTTRIUM_CI_VERSION") ?: "0.0.20-ci" // Use CI version for e2e tests
-    } else {
-        "0.9.69" // Use stable version for local builds
-    }
-
     implementation("com.github.reown-com:yttrium:unspecified")///$yttriumVersion") //unspecified
     implementation("net.java.dev.jna:jna:5.17.0@aar")
 
