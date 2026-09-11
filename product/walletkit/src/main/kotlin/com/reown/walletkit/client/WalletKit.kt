509c3d9c6f55cc2f4959afa6a8efccc614c988bf
jakubuid <jakubwitczak6@gmail.com>
2026-09-11 08:44:46 +0200

    update - remove annotation

--- product/walletkit/src/main/kotlin/com/reown/walletkit/client/WalletKit.kt
+++ product/walletkit/src/main/kotlin/com/reown/walletkit/client/WalletKit.kt
@@ -489,7 +489,6 @@ object WalletKit {
          * signatures alias), and optional collectedData
          * @return Result containing confirm payment response or an error
          */
-        @Suppress("DEPRECATION") // reads signatures as the legacy fallback for data
         suspend fun confirmPayment(
             params: Wallet.Params.ConfirmPayment
         ): Result<Wallet.Model.ConfirmPaymentResponse> {
